fdaccd4e5764ca84af0ccfce168e8e8945feaaee
TrueSaiyan <ufperth@protonmail.com>
2024-02-21 01:19:52 +0800

    Update torrent_downloader.py

--- torrentp/torrent_downloader.py
+++ torrentp/torrent_downloader.py
@@ -2,7 +2,9 @@ from .session import Session
 from .torrent_info import TorrentInfo
 from .downloader import Downloader
 import libtorrent as lt
-
+import asyncio
+import sys
+import time
 
 class TorrentDownloader:
     def __init__(self, file_path, save_path):
@@ -15,7 +17,7 @@ class TorrentDownloader:
         self._add_torrent_params = None
         self._session = Session(self._lt)
 
-    async def start_download(self, download_speed=0, upload_speed=0):
+    async def start_download(self, download_speed=0, upload_speed=0, event=None):
         if self._file_path.startswith('magnet:'):
             self._add_torrent_params = self._lt.parse_magnet_uri(self._file_path)
             self._add_torrent_params.save_path = self._save_path
@@ -31,7 +33,11 @@ class TorrentDownloader:
         self._session.set_upload_limit(upload_speed)
 
         self._file = self._downloader
-        await self._file.download()
+        await event.edit(f"Starting download of {self._file_path}...")
+        await self._file.download(progress_callback=self.progress_callback)
+
+    async def progress_callback(self, status, event):
+        await event.edit(f"Progress: {status.progress * 100:.2f}%")
 
     def __str__(self):
         pass
